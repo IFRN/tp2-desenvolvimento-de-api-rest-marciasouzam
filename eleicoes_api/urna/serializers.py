e6c360e872fcfb0ff081ce29f29da705376add50
MARCIA SOUZA MENDONCA <20242014040011@ifrn.local>
2026-05-13 15:21:45 -0300

    refactor: adições em CandidatoSerializer e EleicaoSerializer.

--- eleicoes_api/urna/serializers.py
+++ eleicoes_api/urna/serializers.py
@@ -12,10 +12,26 @@ class EleicaoSerializer(serializers.ModelSerializer):
         model = Eleicao
         fields = '__all__'
 
+    def get_status_display(self, obj):
+        return obj.get_status_display()
+
+    def get_total_candidatos(self, obj):
+        return obj.candidatos.count()
+
+    def get_total_aptos(self, obj):
+        return obj.aptos.count()
+
 class CandidatoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidato
         fields = '__all__'
+
+        def validate_numero(self, value):
+            if value == 0:
+                raise serializers.ValidationError(
+                    "O número 0 é reservado para voto em branco e não pode ser atribuído a um candidato."
+                )
+            return value
 
 class AptidaoEleitorSerializer(serializers.ModelSerializer):
     class Meta:
